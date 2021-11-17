37fac5cf827afb110805be33611034ea40a2d7f9
Alexandr Smirnov <smirnalexandr@rambler.ru>
2021-11-17 13:20:05 +0300

    Add dataset generation for DETR

--- src/detr_segmentation.py
+++ src/detr_segmentation.py
@@ -4,6 +4,7 @@ import cv2
 from PIL import Image
 import requests
 import matplotlib.pyplot as plt
+import argparse
 #%config InlineBackend.figure_format = 'retina'
 
 #import ipywidgets as widgets
@@ -15,6 +16,10 @@ from torch._C import JITException
 from torchvision.models import resnet50
 import torchvision.transforms as T
 torch.set_grad_enabled(False)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', type=str, default='traffic.mp4')
+args = parser.parse_args()
 
 # COCO classes
 CLASSES = [
@@ -34,6 +39,8 @@ CLASSES = [
     'toothbrush'
 ]
 
+TRANSPORT = ['bicycle', 'car', 'motorcycle', 'bus', 'truck']
+
 # colors for visualization
 COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
           [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
@@ -44,6 +51,23 @@ transform = T.Compose([
     T.ToTensor(),
     T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
+
+def erosion(src):
+    erosion_size = 3
+    element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (erosion_size, erosion_size))
+    
+    erosion_dst = cv2.erode(src, element, iterations=1)
+    return erosion_dst
+
+def dilatation(src):
+    dilatation_size = 3
+    element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (dilatation_size, dilatation_size))
+
+    dilatation_dst = cv2.dilate(src, element, iterations=10)
+    return dilatation_dst
+
+def process(src):
+    return dilatation(erosion(src))
 
 # for output bounding box post-processing
 def box_cxcywh_to_xyxy(x):
@@ -58,26 +82,40 @@ def rescale_bboxes(out_bbox, size):
     b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
     return b
 
-capture = cv2.VideoCapture("../traffic.mp4")
+backSub = cv2.createBackgroundSubtractorMOG2()
+backSub.setHistory(3000)
+backSub.setShadowValue(0)
+
+capture = cv2.VideoCapture(args.input)
 width  = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width`
 height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height`
 
-def plot_results(pil_img, prob, boxes, f_n):
-    plt.figure(figsize=(width/100, height/100))
-    plt.imshow(pil_img)
-    ax = plt.gca()
+def plot_results(frame, prob, boxes, frame_number):
+    yaml_writer = cv2.FileStorage("../segmentation_results/detr/labels/frame"+str(frame_number)+".yml", cv2.FileStorage_WRITE | cv2.FileStorage_FORMAT_YAML)
+    yaml_writer.startWriteStruct("boxes", cv2.FileNode_SEQ)
+
     colors = COLORS * 100
     for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
-        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
-                                   fill=False, color=c, linewidth=1))
+        cl = p.argmax()
+        if (CLASSES[cl] in TRANSPORT):
+            p1 = (int(xmin), int(ymin))
+            p2 = (int(xmax), int(ymax))
+            cv2.rectangle(frame, p1, p2, (0, 255, 0))
+            yaml_writer.startWriteStruct("", cv2.FileNode_MAP)
+            yaml_writer.write("class", CLASSES[cl])
+            yaml_writer.write("x_min", p1[0])
+            yaml_writer.write("y_min", p1[1])
+            yaml_writer.write("x_max", p2[0])
+            yaml_writer.write("y_max", p2[1])
+            yaml_writer.endWriteStruct()
+            #cv2.putText(frame, CLASSES[cl], p1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
         
-        print((xmin, ymin), xmax - xmin, ymax - ymin)
-        #cl = p.argmax()
         #text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
         #ax.text(xmin, ymin, text, fontsize=15,
         #        bbox=dict(facecolor='yellow', alpha=0.5))
-    plt.axis('off')
-    plt.savefig("../segmentation_results/detr/frame"+str(frame_number)+".jpg")
+    yaml_writer.endWriteStruct()
+    yaml_writer.release()
+    cv2.imwrite("../segmentation_results/detr/frames/frame"+str(frame_number)+".png", frame)
 
 model = torch.hub.load('facebookresearch/detr', 'detr_resnet50', pretrained=True)
 model.eval()
@@ -87,7 +125,7 @@ while True:
     ret, frame = capture.read()
     if frame is None:
         break
-
+    
     im = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
     img = transform(im).unsqueeze(0)
@@ -97,7 +135,8 @@ while True:
     keep = probas.max(-1).values > 0.7
 
     bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep], (width, height))
-    plot_results(im, probas[keep], bboxes_scaled, frame_number)
+    cv2.imwrite("../segmentation_results/detr/masks/frame"+str(frame_number)+".png", process(backSub.apply(frame)))
+    plot_results(frame, probas[keep], bboxes_scaled, frame_number)
 
 
     frame_number += 1
