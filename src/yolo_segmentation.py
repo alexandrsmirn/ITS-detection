9e429033555b2f067131a01813353acd6842a032
Alexandr Smirnov <smirnalexandr@rambler.ru>
2021-11-20 18:36:19 +0300

    Add file path argument

--- src/yolo_segmentation.py
+++ src/yolo_segmentation.py
@@ -1,5 +1,11 @@
 import os
+import argparse
 
-command = "python ../yolov5/detect.py --source ../traffic.mp4 --line-thickness 2 --hide-labels --hide-conf --save-txt --project ../segmentation_results/yolo"
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', type=str, default='traffic.mp4')
+args = parser.parse_args()
+
+#command = f"python ../yolov5/detect_new.py --weights yolov5n6.pt --img 1280 --source {args.input} --line-thickness 2 --hide-labels --hide-conf --save-txt --project ../segmentation_results/yolo"
+command = f"python ../yolov5/detect_new.py --source {args.input} --line-thickness 1 --hide-labels --hide-conf --save-txt --project ../segmentation_results/yolo"
 
 os.system(command)
