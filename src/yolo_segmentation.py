ec5a640acb700219efd4309864e64f27c7a6f398
Alexandr Smirnov <smirnalexandr@rambler.ru>
2021-11-27 13:00:39 +0300

    Add box filtration

--- src/yolo_segmentation.py
+++ src/yolo_segmentation.py
@@ -35,37 +35,139 @@ def dilatation(src):
 
 def process(src):
     return dilatation(erosion(src))
+    #return closing(opening(src))
+
+def box_filter(results, iou_treshold=0.45, iosa_treshold=0.5, iou_frist=True, select_bigger=True):
+    def IoU(box1, box2):
+        a1, b1 = box1
+        a2, b2 = box2
+
+        a1x, a1y = a1
+        b1x, b1y = b1
+
+        a2x, a2y = a2
+        b2x, b2y = b2
+
+        area1 = (b1x - a1x) * (b1y - a1y)
+        area2 = (b2x - a2x) * (b2y - a2y)
+
+        xleft = max(a1x, a2x)
+        ytop = max(a1y, a2y)
+        xright = min(b1x, b2x)
+        ybot = min(b1y, b2y)
+
+        w = max(0, xright - xleft)
+        h = max(0, ybot - ytop)
+
+        intersection_area = w * h
+        union_area = area1 + area2 - intersection_area
+        IoU = intersection_area / union_area
+        return IoU
+
+    def IoSA(box1, box2):
+        a1, b1 = box1
+        a2, b2 = box2
+
+        a1x, a1y = a1
+        b1x, b1y = b1
+
+        a2x, a2y = a2
+        b2x, b2y = b2
+
+        area1 = (b1x - a1x) * (b1y - a1y)
+        area2 = (b2x - a2x) * (b2y - a2y)
+
+        xleft = max(a1x, a2x)
+        ytop = max(a1y, a2y)
+        xright = min(b1x, b2x)
+        ybot = min(b1y, b2y)
+
+        w = max(0, xright - xleft)
+        h = max(0, ybot - ytop)
+
+        intersection_area = w * h
+        smallest_area = min(area1, area2)
+
+        return area1 < area2, intersection_area / smallest_area
+
+    keep = []
+    while (not results.empty):
+        max_conf = 0
+        max_conf_idx = 0
+        for row in results.itertuples(index = True): #index?????
+            idx = row[0]   
+            conf  = row[5] 
+            if (conf > max_conf):
+                box = [(round(row[1]), round(row[2])), (round(row[3]), round(row[4]))]
+                max_conf_idx = idx
+        results.drop(labels = max_conf_idx, axis = 0, inplace=True)
+
+        drop_indices = []
+        for row in results.itertuples(index = True):
+            box_to_check = [(round(row[1]), round(row[2])), (round(row[3]), round(row[4]))]
+            if iou_frist:
+                if IoU(box, box_to_check) > iou_treshold:
+                    drop_indices.append(row[0])
+                else:
+                    is_first_smaller, iosa = IoSA(box, box_to_check)
+                    if iosa > iosa_treshold:
+                        if is_first_smaller and select_bigger:
+                            box = box_to_check
+                        drop_indices.append(row[0])
+            else:
+                is_first_smaller, iosa = IoSA(box, box_to_check)
+                if iosa > iosa_treshold:
+                    if is_first_smaller and select_bigger:
+                        box = box_to_check
+                    drop_indices.append(row[0])
+                elif IoU(box, box_to_check) > iou_treshold:
+                    drop_indices.append(row[0])
+
+        keep.append(box)
+        results.drop(labels = drop_indices, axis = 0, inplace=True)
+
+    return keep
 
 
 def write_boxes(results, frame, frame_number):
     frame_name = 'frame_' + str(frame_number)
-    yaml_writer = cv2.FileStorage(txt_path + frame_name + '.yml', cv2.FileStorage_WRITE | cv2.FileStorage_FORMAT_YAML)
-    yaml_writer.startWriteStruct("boxes", cv2.FileNode_SEQ)
+    #yaml_writer = cv2.FileStorage(txt_path + frame_name + '.yml', cv2.FileStorage_WRITE | cv2.FileStorage_FORMAT_YAML)
+    #yaml_writer.startWriteStruct("boxes", cv2.FileNode_SEQ)
+    #for row in results.itertuples(index = False):
+    #    cls = row[6]   
+    #    conf  = row[4] 
+    #    if (cls in TRANSPORT and conf>0.35):
+    #        xmin = round(row[0]) #returns integer
+    #        ymin = round(row[1]) 
+    #        xmax = round(row[2]) 
+    #        ymax = round(row[3]) 
+    #
+    #        #yaml_writer.startWriteStruct("", cv2.FileNode_MAP)
+    #        #yaml_writer.write("class", cls)
+    #        #yaml_writer.write("x_min", xmin)
+    #        #yaml_writer.write("y_min", ymin)
+    #        #yaml_writer.write("x_max", xmax)
+    #        #yaml_writer.write("y_max", ymax)
+    #        #yaml_writer.endWriteStruct()
+    #
+    #        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0))
+    #        cv2.putText(frame, cls, (xmin, ymin), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+    #yaml_writer.endWriteStruct()
+    #yaml_writer.release()
 
-    for row in results.itertuples(index = False):
-        cls = row[6]   
-        conf  = row[4] 
-        if (cls in TRANSPORT):
-            xmin = round(row[0]) #returns integer
-            ymin = round(row[1]) 
-            xmax = round(row[2]) 
-            ymax = round(row[3]) 
-
-            yaml_writer.startWriteStruct("", cv2.FileNode_MAP)
-            yaml_writer.write("class", cls)
-            yaml_writer.write("x_min", xmin)
-            yaml_writer.write("y_min", ymin)
-            yaml_writer.write("x_max", xmax)
-            yaml_writer.write("y_max", ymax)
-            yaml_writer.endWriteStruct()
-
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0))
-    yaml_writer.endWriteStruct()
-    yaml_writer.release()
-
-    cv2.imwrite(save_path + frame_name + '.png', frame)
+    #cv2.imwrite(save_path + frame_name + '.png', frame)
     #cv2.imshow('window_name', frame)
     #cv2.waitKey(10)
+
+    boxes = box_filter(results, 0.7, 0.4)
+
+    for box in boxes:
+        xmin = box[0][0] #returns integer
+        ymin = box[0][1] 
+        xmax = box[1][0] 
+        ymax = box[1][1]
+        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0))
+
     return frame
 
 
@@ -92,6 +194,9 @@ fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+#model = torch.hub.load('ultralytics/yolov5', 'yolov5n6')
+#model.multi_label = True
+model.classes = [1, 2, 3, 5, 7]
 
 frame_number = 0
 while True:
