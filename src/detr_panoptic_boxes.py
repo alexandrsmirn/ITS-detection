310d352c1959c96a4a449961bc9d6e8a662a9bd6
Alexandr Smirnov <smirnalexandr@rambler.ru>
2022-01-08 16:23:55 +0300

    Add box filtering to DETR panoptic

--- src/detr_panoptic_boxes.py
+++ src/detr_panoptic_boxes.py
@@ -79,64 +79,63 @@ def rescale_bboxes(out_bbox, size):
     b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
     return b
 
-#def box_filter(results, confss, iou_treshold=0.3): #iou_treshold=0.5, iosa_treshold=0.8, iou_frist=True, select_bigger=False
-#    def IoU(box1, box2):
-#        a1x, a1y, b1x, b1y = box1.toList()
-#        a2x, a2y, b2x, b2y = box2.toList()
-#
-#        area1 = (b1x - a1x) * (b1y - a1y)
-#        area2 = (b2x - a2x) * (b2y - a2y)
-#
-#        xleft = max(a1x, a2x)
-#        ytop = max(a1y, a2y)
-#        xright = min(b1x, b2x)
-#        ybot = min(b1y, b2y)
-#
-#        w = max(0, xright - xleft)
-#        h = max(0, ybot - ytop)
-#
-#        intersection_area = w * h
-#        union_area = area1 + area2 - intersection_area
-#        IoU = intersection_area / union_area
-#        return IoU
-#
-#    keep = []
-#    confs = []
-#    classes = []
-#    while (not results.empty):
-#        #choose the box with the highest confidence
-#        max_conf = 0
-#        max_conf_idx = 0
-#        for row in results.itertuples(index = True): #index?????
-#            idx = row[0]   
-#            curr_conf = row[5]
-#            if (curr_conf > max_conf):
-#                max_conf = curr_conf
-#                max_conf_idx = idx
-#
-#                box = [round(row[1]), round(row[2]), round(row[3]), round(row[4])]
-#                box_conf  = curr_conf
-#                box_cl = row[7]
-#
-#        results.drop(labels = max_conf_idx, axis = 0, inplace=True)
-#
-#        #remove boxes that intersects with the choosen box
-#        drop_indices = []
-#        for row in results:
-#            row = row.toList()
-#            box_to_check = [round(row[1]), round(row[2]), (round(row[3]), round(row[4]))]
-#            box_to_check_crop = row[8]
-#
-#            
-#
-#            drop_indices.append(row[0])
-#
-#        keep.append(box)
-#        confs.append(box_conf)
-#        classes.append(box_cl)
-#        results.drop(labels = drop_indices, axis = 0, inplace=True)
-#
-#    return keep, confs, classes
+def box_filter(boxes, confs, classes, iou_treshold=0.3): #iou_treshold=0.5, iosa_treshold=0.8, iou_frist=True, select_bigger=False
+    def IoU(box1, box2):
+        a1x, a1y, b1x, b1y = box1
+        a2x, a2y, b2x, b2y = box2
+
+        area1 = (b1x - a1x) * (b1y - a1y)
+        area2 = (b2x - a2x) * (b2y - a2y)
+
+        xleft = max(a1x, a2x)
+        ytop = max(a1y, a2y)
+        xright = min(b1x, b2x)
+        ybot = min(b1y, b2y)
+
+        w = max(0, xright - xleft)
+        h = max(0, ybot - ytop)
+
+        intersection_area = w * h
+        union_area = area1 + area2 - intersection_area
+        IoU = intersection_area / union_area
+        return IoU
+
+    keep = []
+    confs_keep = []
+    classes_keep = []
+    while (not len(boxes) == 0):
+        #choose the box with the highest confidence
+        max_conf = 0
+        max_conf_idx = 0
+        for idx, (curr_box, curr_conf, curr_cl) in enumerate(zip(boxes, confs, classes)):
+            if (curr_conf > max_conf):
+                max_conf = curr_conf
+                max_conf_idx = idx
+
+                box = curr_box
+                box_conf  = curr_conf
+                box_cl = curr_cl
+
+        del boxes[max_conf_idx]
+        del confs[max_conf_idx]
+        del classes[max_conf_idx]
+
+        #remove boxes that intersects with the choosen box
+        drop_indices = []
+        for idx, curr_box in enumerate(boxes):
+            if (IoU(box, curr_box) > iou_treshold):
+                drop_indices.append(idx)
+
+        keep.append(box)
+        confs_keep.append(box_conf)
+        classes_keep.append(box_cl)
+        
+        for index in sorted(drop_indices, reverse=True):
+            del boxes[index]
+            del confs[index]
+            del classes[index]
+
+    return keep, confs_keep, classes_keep
 
 def plot_results(frame, prob, boxes):
     colors = COLORS * 100
@@ -153,6 +152,88 @@ def plot_results(frame, prob, boxes):
     #cv2.imshow("qwe", frame)
     #cv2.waitKey(0)
 
+def cvt_results(prob, boxes):
+    box_list = []
+    conf_list = []
+    class_list = []
+    for p, (xmin, ymin, xmax, ymax) in zip(prob, boxes.tolist()):
+        cl = p.argmax()
+        if (cl < 30 and CLASSES[cl] in TRANSPORT):
+            conf = p[cl].item()
+            box = (int(xmin), int(ymin), int(xmax), int(ymax))
+            box_list.append(box)
+            conf_list.append(conf)
+            class_list.append(cl)
+
+    return box_list, conf_list, class_list
+
+
+def plot_cvt_results_to_file(frame, prob, boxes, panoptic_segm):
+    yaml_writer = cv2.FileStorage("/tmp/detr_segm"+".yml", cv2.FileStorage_WRITE | cv2.FileStorage_FORMAT_YAML)
+    yaml_writer.startWriteStruct("boxes", cv2.FileNode_SEQ)
+
+    #box_list, conf_list, class_list = cvt_results(prob, boxes)
+    keep, confs, clas = box_filter(*cvt_results(prob, boxes))
+    for box, conf, cl in zip(keep, confs, clas):        
+        xmin, ymin, xmax, ymax = box
+        p1 = (xmin, ymin)
+        p2 = (xmax, ymax)
+        cv2.rectangle(frame, p1, p2, (0, 255, 0))
+
+        center_x = int(0.5*(xmax + xmin))
+        center_y = int(0.5*(ymax + ymin))
+
+        instance_id = panoptic_segm[center_y, center_x]
+
+        yaml_writer.startWriteStruct("", cv2.FileNode_MAP)
+        yaml_writer.write("instance_id", instance_id)
+        yaml_writer.write("conf", conf)
+        yaml_writer.write("class", CLASSES[cl])
+        yaml_writer.write("x_min", xmin)
+        yaml_writer.write("y_min", ymin)
+        yaml_writer.write("x_max", xmax)
+        yaml_writer.write("y_max", ymax)
+        yaml_writer.endWriteStruct()
+
+    yaml_writer.endWriteStruct()
+    yaml_writer.release()
+    cv2.imshow('qwe', frame)
+    cv2.waitKey(10)
+
+
+def plot_results_to_file(frame, prob, boxes, panoptic_segm):
+    yaml_writer = cv2.FileStorage("/tmp/detr_segm"+".yml", cv2.FileStorage_WRITE | cv2.FileStorage_FORMAT_YAML)
+    yaml_writer.startWriteStruct("boxes", cv2.FileNode_SEQ)
+
+    colors = COLORS * 100
+    for p, (xmin, ymin, xmax, ymax)in zip(prob, boxes.tolist()):
+        cl = p.argmax()
+        if (cl < 30 and CLASSES[cl] in TRANSPORT):
+            conf = p[cl].item()
+            p1 = (int(xmin), int(ymin))
+            p2 = (int(xmax), int(ymax))
+            cv2.rectangle(frame, p1, p2, (0, 255, 0))
+
+            center_x = int(0.5*(xmax + xmin))
+            center_y = int(0.5*(ymax + ymin))
+
+            instance_id = panoptic_segm[center_y, center_x]
+
+            yaml_writer.startWriteStruct("", cv2.FileNode_MAP)
+            yaml_writer.write("instance_id", instance_id)
+            yaml_writer.write("conf", conf)
+            yaml_writer.write("class", CLASSES[cl])
+            yaml_writer.write("x_min", p1[0])
+            yaml_writer.write("y_min", p1[1])
+            yaml_writer.write("x_max", p2[0])
+            yaml_writer.write("y_max", p2[1])
+            yaml_writer.endWriteStruct()
+
+    yaml_writer.endWriteStruct()
+    yaml_writer.release()
+    cv2.imshow('qwe', frame)
+    cv2.waitKey(10)
+
 model, postprocessor = torch.hub.load('facebookresearch/detr', 'detr_resnet101_panoptic', pretrained=True, return_postprocessor=True, num_classes=250, threshold=0.4)
 model.eval()
 
@@ -168,7 +249,7 @@ confs = probas[keep].max(-1).values
 
 # convert boxes from [0; 1] to image scales
 bboxes_scaled = rescale_bboxes(out['pred_boxes'][0, keep], im.size)
-plot_results(frame_cv, probas[keep], bboxes_scaled)
+#plot_results(frame_cv, probas[keep], bboxes_scaled)
 
 # the post-processor expects as input the target size of the predictions (which we set here to the image size)
 result = postprocessor(out, torch.as_tensor(img.shape[-2:]).unsqueeze(0))[0]
@@ -183,23 +264,37 @@ print(panoptic_seg.shape)
 # We retrieve the ids corresponding to each mask
 panoptic_seg_id = rgb2id(panoptic_seg)
 print(panoptic_seg_id.shape)
+new_shape = panoptic_seg_id.shape
 
 # Finally we color each mask individually
-panoptic_seg[:, :, :] = 0
+panoptic_seg = numpy.zeros(new_shape, dtype=numpy.uint8)
+mask_list = []
 for id in range(panoptic_seg_id.max() + 1):
   segm_info = result['segments_info'][id]
-  #print(segm_info)
-
   if segm_info['isthing'] is True and CLASSES[segm_info['category_id']] in TRANSPORT:
-    panoptic_seg[panoptic_seg_id == id] = numpy.asarray((1, 1, 1)) * 255
-  else:
-    panoptic_seg[panoptic_seg_id == id] = numpy.asarray((0, 0, 0)) * 255
+    mask_list.append(id)
+
+mask_id_max = len(mask_list)
+print(mask_id_max)
+mask_step = 255/(mask_id_max)
+mask_id = 0
+for id in mask_list:
+  mask_id = mask_id + mask_step
+  if mask_id > 255:
+      mask_id = 255
+  panoptic_seg[panoptic_seg_id == id] = mask_id
 
 
-mask_cv = cv2.resize(cv2.cvtColor(panoptic_seg, cv2.COLOR_RGB2BGR), (1280, 720))
-output_frame = cv2.addWeighted(frame_cv, 0.55, mask_cv, 0.45, 0.0)
+panoptic_seg = cv2.resize(panoptic_seg, (1280, 720))
+mask_cv = cv2.cvtColor(panoptic_seg, cv2.COLOR_GRAY2BGR)
+
+plot_cvt_results_to_file(frame_cv, probas[keep], bboxes_scaled, panoptic_seg)
+output_frame = cv2.addWeighted(frame_cv, 0.45, mask_cv, 0.55, 0.0)
+#output_frame = mask_cv
+
 cv2.imshow('qwe', output_frame)
 cv2.imwrite('/tmp/detr_segm.png', output_frame)
 cv2.waitKey(0)
 
 #nearest neighbour, чтобы не придумывал новые значения!!!
+#спросить про ситуацию, когда бокс не детектится, а маска генерится. интересен ли нам такой случай???
