e55faf533c10511ad72e29ed4e5e843e21ee6808
Alexandr Smirnov <smirnalexandr@rambler.ru>
2022-01-11 15:32:08 +0300

    Refactor and bug fix

--- src/detr_panoptic_generator.py
+++ src/detr_panoptic_generator.py
@@ -81,11 +81,18 @@ def rescale_bboxes(out_bbox, size):
     b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32, device=device)
     return b
 
+class Box:
+    def __init__(self, rect, conf, cls, crop, inst_id):
+        self.rect = rect
+        self.conf = conf
+        self.cls = cls
+        self.crop = crop
+        self.inst_id = inst_id
 
-def box_filter(boxes, confs, classes, crops, iou_treshold=0.3, iosa_treshold=0.4): #iou_treshold=0.5, iosa_treshold=0.8, iou_frist=True, select_bigger=False
+def box_filter(boxes: list[Box], iou_treshold=0.3, iosa_treshold=0.4): #iou_treshold=0.5, iosa_treshold=0.8, iou_frist=True, select_bigger=False
     def IoU(box1, box2):
-        a1x, a1y, b1x, b1y = box1
-        a2x, a2y, b2x, b2y = box2
+        a1x, a1y, b1x, b1y = box1.rect
+        a2x, a2y, b2x, b2y = box2.rect
 
         area1 = (b1x - a1x) * (b1y - a1y)
         area2 = (b2x - a2x) * (b2y - a2y)
@@ -104,8 +111,8 @@ def box_filter(boxes, confs, classes, crops, iou_treshold=0.3, iosa_treshold=0.4
         return IoU
 
     def IoSA(box1, box2):
-        a1x, a1y, b1x, b1y = box1
-        a2x, a2y, b2x, b2y = box2
+        a1x, a1y, b1x, b1y = box1.rect
+        a2x, a2y, b2x, b2y = box2.rect
 
         area1 = (b1x - a1x) * (b1y - a1y)
         area2 = (b2x - a2x) * (b2y - a2y)
@@ -123,81 +130,63 @@ def box_filter(boxes, confs, classes, crops, iou_treshold=0.3, iosa_treshold=0.4
 
         return area1 < area2, intersection_area / smallest_area
 
-    def is_on_border(box, box_crop, eps):
-        if box_crop == 1:
-            return abs(box[0] - top_left1[0]) < eps or \
-                abs(box[1] - top_left1[1]) < eps or \
-                abs(box[2] - bot_right1[0]) < eps or \
-                abs(box[3] - bot_right1[1]) < eps
+    def is_on_border(box, eps):
+        if box.crop == 1:
+            return abs(box.rect[0] - top_left1[0]) < eps or \
+                abs(box.rect[1] - top_left1[1]) < eps or \
+                abs(box.rect[2] - bot_right1[0]) < eps or \
+                abs(box.rect[3] - bot_right1[1]) < eps
         #elif box_crop == 2:
         #    return abs(box[0] - top_left2[0]) < eps or \
         #        abs(box[2] - bot_right2[0]) < eps or \
         #        abs(box[3] - bot_right2[1]) < eps
 
     def is_in_first_crop(box):
-        return  box[0] > top_left1[0] and \
-                box[1] > top_left1[1] and \
-                box[2] < bot_right1[0] and \
-                box[3] < bot_right1[1]
+        return  box.rect[0] > top_left1[0] and \
+                box.rect[1] > top_left1[1] and \
+                box.rect[2] < bot_right1[0] and \
+                box.rect[3] < bot_right1[1]
     
     keep = []
-    confs_keep = []
-    classes_keep = []
     while (not len(boxes) == 0):
         #choose the box with the highest confidence
         max_conf = 0
         max_conf_idx = 0
-        for idx, (curr_box, curr_conf, curr_cl, curr_crop) in enumerate(zip(boxes, confs, classes, crops)):
+        for idx, curr_box in enumerate(boxes):
+            curr_conf = curr_box.conf
             if (curr_conf > max_conf):
                 max_conf = curr_conf
                 max_conf_idx = idx
 
                 box = curr_box
-                box_conf  = curr_conf
-                box_cl = curr_cl
-                box_crop = curr_crop
 
-        del boxes[max_conf_idx]
-        del confs[max_conf_idx]
-        del classes[max_conf_idx]
-        del crops[max_conf_idx]
+        del boxes[max_conf_idx] #не удалится ли???
 
-        if box_crop == 3 and is_in_first_crop(box): #remove boxes that are on the 3 crop but not in 1
+        if box.crop == 3 and is_in_first_crop(box): #remove boxes that are on the 3 crop but not in 1
             continue
 
         #remove boxes that intersects with the choosen box
         drop_indices = []
-        for curr_idx, (curr_box, curr_conf, curr_cl, curr_crop) in enumerate(zip(boxes, confs, classes, crops)):
+        for curr_idx, curr_box in enumerate(boxes):
             iou = IoU(box, curr_box)
             is_first_smaller, iosa = IoSA(box, curr_box)
             if iosa > iosa_treshold:
-                if box_crop == 3 and curr_crop == 1:
+                if box.crop == 3 and curr_box.crop == 1:
                     box = curr_box
-                    box_conf  = curr_conf
-                    box_cl = curr_cl
-                    box_crop = curr_crop
-                elif box_crop == curr_crop:
+                elif box.crop == curr_box.crop:
                     if iou < iou_treshold and iosa < 0.99:
                         continue
                     elif not is_first_smaller:
                         box = curr_box
-                        box_conf  = curr_conf
-                        box_cl = curr_cl
-                        box_crop = curr_crop
                 
                 drop_indices.append(curr_idx)
 
         keep.append(box)
-        confs_keep.append(box_conf)
-        classes_keep.append(box_cl)
         
         for index in sorted(drop_indices, reverse=True):
             del boxes[index]
-            del confs[index]
-            del classes[index]
-            del crops[index]
 
-    return keep, confs_keep, classes_keep
+    return keep
 
 
 def create_mask(result, shape):
@@ -209,10 +198,12 @@ def create_mask(result, shape):
 
     panoptic_seg = numpy.zeros(new_shape, dtype=numpy.uint8)
     mask_list = []
-    for id in range(panoptic_seg_id.max() + 1):
-        segm_info = result['segments_info'][id]
-        if segm_info['isthing'] is True and CLASSES[segm_info['category_id']] in TRANSPORT:
-            mask_list.append(id)
+    max_idx = panoptic_seg_id.max()
+    if not max_idx == 0:
+        for id in range(max_idx + 1):
+            segm_info = result['segments_info'][id]
+            if segm_info['isthing'] is True and CLASSES[segm_info['category_id']] in TRANSPORT:
+                mask_list.append(id)
 
     mask_id_max = len(mask_list)
     print(mask_id_max)
@@ -230,45 +221,41 @@ def create_mask(result, shape):
     return panoptic_seg
 
 
-def cvt_results(prob, boxes, crop_num):
+def cvt_results(prob, boxes, crop_num, panoptic_segm):
     box_list = []
-    conf_list = []
-    class_list = []
-    crop_list = []
     for p, (xmin, ymin, xmax, ymax) in zip(prob, boxes.tolist()):
         cl = p.argmax()
-        if (cl < 30 and CLASSES[cl] in TRANSPORT):
+        if (cl < 12 and CLASSES[cl] in TRANSPORT):
             conf = p[cl].item()
-            box = (int(xmin), int(ymin), int(xmax), int(ymax))
+            rect = (int(xmin), int(ymin), int(xmax), int(ymax))
+
+            center_x = int(0.5*(xmax + xmin))
+            center_y = int(0.5*(ymax + ymin))
+            instance_id = panoptic_segm[center_y, center_x]
+
+            box = Box(rect, conf, cl, crop_num, instance_id)
             box_list.append(box)
-            conf_list.append(conf)
-            class_list.append(cl)
-            crop_list.append(crop_num)
 
-    return box_list, conf_list, class_list, crop_list
+    return box_list
 
 
-def plot_cvt_results_to_file(frame, keep, confs, clas, frame_number, panoptic_segm, mask):
+def plot_cvt_results_to_file(frame, keep, frame_number, mask):
     yaml_writer = cv2.FileStorage("/tmp/detr_segm/labels/frame-"+str(frame_number)+".yml", cv2.FileStorage_WRITE | cv2.FileStorage_FORMAT_YAML)
     yaml_writer.startWriteStruct("boxes", cv2.FileNode_SEQ)
 
-    for box, conf, cl in zip(keep, confs, clas):        
-        xmin, ymin, xmax, ymax = box
+    for box in keep:        
+        xmin, ymin, xmax, ymax = box.rect
         p1 = (xmin, ymin)
         p2 = (xmax, ymax)
 
-        center_x = int(0.5*(xmax + xmin))
-        center_y = int(0.5*(ymax + ymin))
-
-        instance_id = panoptic_segm[center_y, center_x]
-        if instance_id == 0:
+        if box.inst_id == 0:
             continue
 
         cv2.rectangle(frame, p1, p2, (0, 255, 0))
         yaml_writer.startWriteStruct("", cv2.FileNode_MAP)
-        yaml_writer.write("instance_id", instance_id)
-        yaml_writer.write("conf", conf)
-        yaml_writer.write("class", CLASSES[cl])
+        yaml_writer.write("instance_id", box.inst_id)
+        yaml_writer.write("conf", box.conf)
+        yaml_writer.write("class", CLASSES[box.cls])
         yaml_writer.write("x_min", xmin)
         yaml_writer.write("y_min", ymin)
         yaml_writer.write("x_max", xmax)
@@ -295,7 +282,8 @@ top_left1 = (490, 20)
 crop_size1 = (295, 150)
 bot_right1 = (top_left1[0] + crop_size1[0], top_left1[1] + crop_size1[1])
 
-frame_number = 6255
+#frame_number = 6255
+frame_number = 6431
 while True:
     frame_cv = cv2.imread("/home/alex/prog/cv/prepared_datasets/Carla-final/from_0_camera/frame-" + str(frame_number) + ".png")
     if frame_cv is None:
@@ -315,10 +303,12 @@ while True:
     keep_1 = probas_1.max(-1).values > 0.85 #0.9
     confs_1 = probas_1[keep_1].max(-1).values
     bboxes_scaled_1 = rescale_bboxes(out_1['pred_boxes'][0, keep_1], im_1.size) + torch.tensor([top_left1[0], top_left1[1], top_left1[0], top_left1[1]], dtype=torch.float32, device=device)
-    box_list_1, conf_list_1, class_list_1, crop_list_1 = cvt_results(probas_1[keep_1], bboxes_scaled_1, 1)
 
     result_1 = postprocessor(out_1, torch.as_tensor(img_1.shape[-2:]).unsqueeze(0))[0]
     panoptic_seg_1 = create_mask(result_1, crop_size1)
+    panoptic_seg_1_scaled = numpy.zeros((720, 1280), dtype=numpy.uint8)
+    panoptic_seg_1_scaled[top_left1[1] : top_left1[1] + crop_size1[1], top_left1[0] : top_left1[0] + crop_size1[0]] = panoptic_seg_1
+    box_list_1 = cvt_results(probas_1[keep_1], bboxes_scaled_1, 1, panoptic_seg_1_scaled)
 
     del img_1
     del out_1
@@ -335,18 +325,16 @@ while True:
 
     result_3 = postprocessor(out_3, torch.as_tensor(img_3.shape[-2:]).unsqueeze(0))[0]
     panoptic_seg_3 = create_mask(result_3, (1280, 720))
-
-    box_list_3, conf_list_3, class_list_3, crop_list_3 = cvt_results(probas_3[keep_3], bboxes_scaled_3, 3)
+    box_list_3 = cvt_results(probas_3[keep_3], bboxes_scaled_3, 3, panoptic_seg_3)
 
     box_list = box_list_3 + box_list_1
-    conf_list = conf_list_3 + conf_list_1
-    class_list = class_list_3 + class_list_1
-    crop_list = crop_list_3 + crop_list_1
+
+    #TODO check
     panoptic_seg_3[top_left1[1] : top_left1[1] + crop_size1[1], top_left1[0] : top_left1[0] + crop_size1[0]] = panoptic_seg_1
 
     mask_cv = cv2.cvtColor(panoptic_seg_3, cv2.COLOR_GRAY2BGR)
-    box_keep, confs_keep, class_keep = box_filter(box_list, conf_list, class_list, crop_list)
-    plot_cvt_results_to_file(frame_cv, box_keep, confs_keep, class_keep, frame_number, panoptic_seg_3, mask_cv)
+    box_keep = box_filter(box_list)
+    plot_cvt_results_to_file(frame_cv, box_keep, frame_number, mask_cv)
 
     output_frame = cv2.addWeighted(frame_cv, 0.45, mask_cv, 0.55, 0.0)
     #output_frame = mask_cv
